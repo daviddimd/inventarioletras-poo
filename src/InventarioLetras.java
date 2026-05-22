6952f8c21f3f822fc5f444473e5cda1af202503a
David munoz <davidignacio5000@gmail.com>
2026-05-21 23:42:02 -0400

    Constructor listo

--- src/InventarioLetras.java
+++ src/InventarioLetras.java
@@ -6,6 +6,29 @@ public class InventarioLetras {
         private static final int ABECEDARIO_TAMANIO = 26;
 
         public InventarioLetras(String data) {
+            this.contadores = new int[ABECEDARIO_TAMANIO];
+            this. totalCount = 0;
+            this.nonZeroCount = 0;
+
+            if (data != null){
+                String textoMinusculas = data.toLowerCase();
+
+                for (int i = 0; i < textoMinusculas.length(); i++){
+                    char caracter = textoMinusculas.charAt(i);
+
+                    if(caracter >= 'a' && caracter <= 'z') {
+                        int indice = caracter - 'a';
+                        this.contadores[indice]++;
+                        this.totalCount++;
+                    }
+                }
+                for (int i = 0; i < ABECEDARIO_TAMANIO; i++){
+                    if(this.contadores[i] > 0) {
+                        this.nonZeroCount++;
+                    }
+                }
+            }
+
         }
 
         public int get(char letra) {
