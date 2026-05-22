885910ca82ef4730ca38dcc30ce2e8f736db7e07
David munoz <davidignacio5000@gmail.com>
2026-05-21 23:33:17 -0400

    Estructura base de la clase inventarioletras

---
+++ src/InventarioLetras.java
@@ -0,0 +1,57 @@
+public class InventarioLetras {
+
+        private int[] contadores;
+        private int totalCount;
+        private int nonZeroCount;
+        private static final int ABECEDARIO_TAMANIO = 26;
+
+        public InventarioLetras(String data) {
+        }
+
+        public int get(char letra) {
+            return 0;
+        }
+
+        public int size() {
+            return 0;
+        }
+
+        public boolean isEmpty() {
+            return false;
+        }
+
+        public String toString() {
+            return null;
+        }
+
+        public char encriptarCesar(char letra) {
+            return ' ';
+        }
+
+        public char desencriptarCesar(char letra) {
+            return ' ';
+        }
+
+        public String encriptarPalabra(String palabra, int desplazamiento) {
+            return null;
+        }
+
+        public String desencriptarPalabra(String palabra, int desplazamiento) {
+            return null;
+        }
+
+        public void set(char letra, int valor) {
+        }
+
+        public InventarioLetras add(InventarioLetras otro) {
+            return null;
+        }
+
+        public InventarioLetras amplifies(int n) {
+            return null;
+        }
+
+        public InventarioLetras subtract(InventarioLetras otro) {
+            return null;
+        }
+    }
